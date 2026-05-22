fb562f4977fd73801dec88402be2d3e3a814c84e
Bipin Yadav <bipin@example.com>
2026-05-23 01:49:05 +0530

    Fix duplicate i18next initialization

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,26 +3,28 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n
-    .use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        supportedLngs: ['en', 'hi'],
-        nonExplicitSupportedLngs: true,
-        load: 'languageOnly',
-        fallbackLng: 'en',
-        debug: true, // Set to false in production
-        interpolation: {
-            escapeValue: false, // React already safes from xss
-        },
-        backend: {
-            loadPath: '/locales/{{lng}}/translation.json',
-        },
-        detection: {
-            order: ['localStorage', 'navigator'],
-            caches: ['localStorage'],
-        },
-    });
+if (!i18n.isInitialized) {
+    i18n
+        .use(Backend)
+        .use(LanguageDetector)
+        .use(initReactI18next)
+        .init({
+            supportedLngs: ['en', 'hi'],
+            nonExplicitSupportedLngs: true,
+            load: 'languageOnly',
+            fallbackLng: 'en',
+            debug: process.env.NODE_ENV !== 'production',
+            interpolation: {
+                escapeValue: false,
+            },
+            backend: {
+                loadPath: '/locales/{{lng}}/translation.json',
+            },
+            detection: {
+                order: ['localStorage', 'navigator'],
+                caches: ['localStorage'],
+            },
+        });
+}
 
 export default i18n;
